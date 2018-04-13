21bf628eab2243b8a6ffd03c7137d1f78ad26fa8
Peter Lee <peter.lee@berkeley.edu>
2018-04-12 21:24:02 -0700

    Added timeline

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,13 +22,8 @@
     $('.materialboxed').materialbox();
     $('.modal').modal();
     $('.scrollspy').scrollSpy();
-    $('.datepicker').datepicker();
     $('.tabs').tabs();
-    $('.timepicker').timepicker();
     $('.tooltipped').tooltip();
-    $('select').not('.disabled').formSelect();
-    $('.sidenav').sidenav();
-    $('.tap-target').tapTarget();
     $('input.autocomplete').autocomplete({
       data: {"Apple": null, "Microsoft": null, "Google": 'http://placehold.it/250x250'},
     });
@@ -39,45 +34,6 @@
       $('#tabs-swipe-demo').tabs({ 'swipeable': true });
     }
 
-    // Chips
-    $('.chips').chips();
-    $('.chips-initial').chips({
-      readOnly: true,
-      data: [{
-        tag: 'Apple',
-      }, {
-        tag: 'Microsoft',
-      }, {
-        tag: 'Google',
-      }]
-    });
-    $('.chips-placeholder').chips({
-      placeholder: 'Enter a tag',
-      secondaryPlaceholder: '+Tag',
-    });
-    $('.chips-autocomplete').chips({
-      autocompleteOptions: {
-        data: {
-          'Apple': null,
-          'Microsoft': null,
-          'Google': null
-        }
-      },
-    });
-
-    // Fab
-    $('.fixed-action-btn').floatingActionButton();
-    $('.fixed-action-btn.horizontal').floatingActionButton({
-      direction: 'left'
-    });
-    $('.fixed-action-btn.click-to-toggle').floatingActionButton({
-      direction: 'left',
-      hoverEnabled: false
-    });
-    $('.fixed-action-btn.toolbar').floatingActionButton({
-      toolbarEnabled: true
-    });
-
-
+    $('.slider-container').css('height', $('#cover-slider').height());
   }); // end of document ready
 })(jQuery); // end of jQuery name space
